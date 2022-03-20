0cbcb48b84a5f28467c5cf6cef2e78b064c34f2b
PawelPalasinski <pawel.palasinski@gmail.com>
2022-03-20 02:00:47 +0100

    name change for menu (now mobile-menu)

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,5 +1,5 @@
 function myFunction() {
-  var x = document.getElementById("menu");
+  var x = document.getElementById("mobile-menu");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
